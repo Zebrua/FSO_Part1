dde8377790bd6cb2d71c321d17334d3b45eba014
Zebrua <141924342+Zebrua@users.noreply.github.com>
2024-07-31 13:34:09 +0300

    Create 1-1_App.jsx

---
+++ 1-1_App.jsx
@@ -0,0 +1,42 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+};
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.amount}</p>
+    </div>
+  )
+};
+const Total = (props) => {
+  return (
+    <div>
+      <p>Total number of exercises {props.number}</p>
+    </div>
+  )
+};
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content name={part1} amount={exercises1} />
+      <Content name={part2} amount={exercises2} />
+      <Content name={part3} amount={exercises3} />
+      <Total number={exercises1+exercises2+exercises3} />
+    </div>
+  )
+}
+
+export default App
